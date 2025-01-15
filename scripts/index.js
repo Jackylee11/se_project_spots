b220ac3f8cd2fd746056799aa4cfd328fc8e1746
Jacky <jackylee1993@yahoo.com>
2025-01-14 18:07:08 -0600

    corrected mistakes java/html

--- scripts/index.js
+++ scripts/index.js
@@ -2,26 +2,33 @@ const initalCards = [
   {
     name: "Val Thorens",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+    alt: "Val Thorens Img",
   },
+
   {
     name: "Restaurant terrace",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+    alt: "Restaurant Img",
   },
   {
     name: "An outdoor cafe",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+    alt: "Cafe Img",
   },
   {
     name: "A very long bridge, over the forest and through the trees",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+    alt: "Long Bridge Img",
   },
   {
     name: "Tunnel with morning light",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+    alt: "Tunnel Img",
   },
   {
     name: "Mountain house",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+    alt: "Mountain House Img",
   },
 ];
 
@@ -49,6 +56,7 @@ function getCardElement(data) {
 
   cardNameEl.textContent = data.name;
   cardLinkEl.src = data.link;
+  cardLinkEl.alt = data.alt;
 
   return cardElement;
 }
